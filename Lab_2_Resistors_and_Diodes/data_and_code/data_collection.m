debf2a485cbdb3622921d879c555e47a39eb7cbf
Vienna Scheyer <vscheyer@comcast.net>
2019-02-13 11:56:06 -0500

    More work on plots, need to get theoretical fits correct

--- Lab_2_Resistors_and_Diodes/data_and_code/data_collection.m
+++ Lab_2_Resistors_and_Diodes/data_and_code/data_collection.m
@@ -14,46 +14,48 @@ figure
 semilogy(Vin_Exp1_2, Iout_Exp1_2, 'r');
 
 %% Experiment 2
-
+load('collected_data.mat');
 % theoretical
-Is = 0.75;
-Ut = 0.25
-V = linspace(0.25, 0.75, length(Vin_Exp2_100));
-% I = zeros(length(Vin_Exp2_100));
+Is = 3 * 10^(-15);
+Ut = 0.025;
+V = linspace(0, 3, length(Vin_Exp2_100));
+
 for i = 1:length(Vin_Exp2_100)
-    I(1,i) = Is + exp(V(i)/Ut);
+    I(1,i) = Is * exp(V(i)/Ut) + V(i)/100;
 end
 
-figure
-semilogy(Vin_Exp2_100, Iout_Exp2_100);
-hold on
-semilogy(Vin_Exp2_100, I);
-hold on
-semilogy(Vin_Exp2_1K, Iout_Exp2_1K);
-hold on
-semilogy(Vin_Exp2_10K, Iout_Exp2_10K);
-xlabel('Vin (V)');
-ylabel('Iout (A)');
-title('Semilogarithmic Plot: Resistor and Transistor in Series');
-legend('R=100', 'R=1000', 'R=10000');
-ylim([10e-9, 10e-1]);
-xlim([0.3, 3.1]);
+
 
 figure
-subplot(3,1,1);
-plot(Vin_Exp2_100, Iout_Exp2_100);
-title('Linear Plot: Resistor and Transistor in Series, R=100');
-xlabel('Vin (V)');
-ylabel('Iout (A)');
-subplot(3,1,2);
-plot(Vin_Exp2_1K, Iout_Exp2_1K);
-title('Linear Plot: Resistor and Transistor in Series, R=1K');
-xlabel('Vin (V)');
-ylabel('Iout (A)');
-subplot(3,1,3);
-plot(Vin_Exp2_10K, Iout_Exp2_10K);
-title('Linear Plot: Resistor and Transistor in Series, R=10K');
-xlabel('Vin (V)');
-ylabel('Iout (A)');
+plot(Vin_Exp2_100, Iout_Exp2_100, 'r');
+figure
+plot(V, I, 'b');
+% hold on
+% semilogy(Vin_Exp2_1K, Iout_Exp2_1K, 'b');
+% hold on
+% semilogy(Vin_Exp2_10K, Iout_Exp2_10K, 'b');
+% xlabel('Vin (V)');
+% ylabel('Iout (A)');
+title('Semilogarithmic Plot: Resistor and Transistor in Series');
+legend('R=100', 'R=100 theoretical');
+% ylim([10e-9, 10e-1]);
+% xlim([0.3, 3.1]);
+
+% figure
+% subplot(3,1,1);
+% plot(Vin_Exp2_100, Iout_Exp2_100);
+% title('Linear Plot: Resistor and Transistor in Series, R=100');
+% xlabel('Vin (V)');
+% ylabel('Iout (A)');
+% subplot(3,1,2);
+% plot(Vin_Exp2_1K, Iout_Exp2_1K);
+% title('Linear Plot: Resistor and Transistor in Series, R=1K');
+% xlabel('Vin (V)');
+% ylabel('Iout (A)');
+% subplot(3,1,3);
+% plot(Vin_Exp2_10K, Iout_Exp2_10K);
+% title('Linear Plot: Resistor and Transistor in Series, R=10K');
+% xlabel('Vin (V)');
+% ylabel('Iout (A)');
 
 
