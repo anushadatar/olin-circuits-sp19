b3d74d0a4dd1c1d63f1935eab8f936e6248682d4
Vienna Scheyer <vscheyer@comcast.net>
2019-02-15 15:57:45 -0500

    More work on experiment 2

--- Lab_2_Resistors_and_Diodes/data_and_code/Just_exp1.m
+++ Lab_2_Resistors_and_Diodes/data_and_code/Just_exp1.m
@@ -3,7 +3,7 @@ clear
 load('collected_data.mat');
 
 figure;
-semilogy( Vout_Exp1_1,Iin_Exp1_1,'*b','MarkerSize',2) %current-voltahe
+semilogy( Vout_Exp1_1,Iin_Exp1_1,'*b','MarkerSize',2) %current-voltage
 hold on;
 semilogy(Vin_Exp1_2,Iout_Exp1_2, '*r','MarkerSize',2) %voltage-current
 hold on;
--- Lab_2_Resistors_and_Diodes/data_and_code/data_collection.m
+++ Lab_2_Resistors_and_Diodes/data_and_code/data_collection.m
@@ -14,11 +14,12 @@ figure
 semilogy(Vin_Exp1_2, Iout_Exp1_2, 'r');
 
 %% Experiment 2
+clear
 load('collected_data.mat');
 % R=100
 p100 = polyfit(Vin_Exp2_100, log(Iout_Exp2_100), 1);
-Ut100 = 1/p100(1);
-Is100 = exp(p100(2));
+Ut100 = real(1/p100(1));
+Is100 = real(exp(p100(2)));
 
 p1K = polyfit(Vin_Exp2_1K, log(Iout_Exp2_1K), 1);
 Ut1K = 1/p1K(1);
@@ -28,54 +29,54 @@ p10K = polyfit(Vin_Exp2_100, log(Iout_Exp2_100), 1);
 Ut10K = 1/p10K(1);
 Is10K = exp(p10K(2));
 
-Ion100 = Ut100/100;
+Ion100 = Ut100/100
 Ion1K = Ut1K/1000;
 Ion10K = Ut10K/10000;
 
 Von100 = Ut100 * log(Ion100/Is100);
+VonIndex100 = 41;
 Von1K = Ut1K * log(Ion1K/Is1K);
 Von10K = Ut10K * log(Ion10K/Is10K);
 
 
 V = linspace(0, 3, length(Vin_Exp2_100));
 
-for i = 1:length(Von100)
-    I100(1,i) = Is100 * exp(V(i)/Ut100);
-end
-for l = (length(Von100+1 : end))
-    I100((1,i) = V(i)
+for l = (1 : 41)
+    I100(l) = Is100 * exp(V(l)/Ut100);
 end
 
-for i = 1:length(Vin_Exp2_1K)
-    I1K(1,i) = Is1K * exp(V(i)/Ut1K) + V(i)/1000;
-end
-
-for i = 1:length(Vin_Exp2_1K)
-    I10K(1,i) = Is10K * exp(V(i)/Ut10K) + V(i)/10000;
-end
+% for i = 1:length(Vin_Exp2_1K)
+%     I1K(1,i) = Is1K * exp(V(i)/Ut1K) + V(i)/1000;
+% end
+% 
+% for i = 1:length(Vin_Exp2_1K)
+%     I10K(1,i) = Is10K * exp(V(i)/Ut10K) + V(i)/10000;
+% end
 
 figure
 semilogy(Vin_Exp2_100, Iout_Exp2_100, 'o');
 hold on
-semilogy(V, I100, 'b');
-hold on
-semilogy(Vin_Exp2_1K, Iout_Exp2_1K, 'o');
-hold on
-semilogy(V(1:51), I1K);
-hold on
-semilogy(Vin_Exp2_10K, Iout_Exp2_10K, 'o');
-hold on
-semilogy(V(1:51), I10K);
-xlabel('Vin (V)');
-ylabel('Iout (A)');
-title('Semilogarithmic Plot: Resistor and Transistor in Series');
-legend('R=100', 'R=100 theoretical');
+semilogy(V(1:41), I100, 'b');
+% hold on
+% semilogy(Vin_Exp2_1K, Iout_Exp2_1K, 'o');
+% hold on
+% semilogy(V(1:51), I1K);
+% hold on
+% semilogy(Vin_Exp2_10K, Iout_Exp2_10K, 'o');
+% hold on
+% semilogy(V(1:51), I10K);
+% xlabel('Vin (V)');
+% ylabel('Iout (A)');
+% title('Semilogarithmic Plot: Resistor and Transistor in Series');
+% legend('R=100', 'R=100 theoretical');
 % ylim([10e-9, 10e-1]);
 % xlim([0.3, 3.1]);
 
-% figure
-% subplot(3,1,1);
-% plot(Vin_Exp2_100, Iout_Exp2_100);
+figure
+subplot(3,1,1);
+plot(Vin_Exp2_100, Iout_Exp2_100);
+hold on
+plot(V(41), I100);
 % title('Linear Plot: Resistor and Transistor in Series, R=100');
 % xlabel('Vin (V)');
 % ylabel('Iout (A)');
