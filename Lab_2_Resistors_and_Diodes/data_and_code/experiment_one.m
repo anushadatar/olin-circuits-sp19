8dc0d635a636c3db95e1bdae6bd60529ee39a3b0
Anusha Datar <anushadatar@gmail.com>
2019-02-16 17:07:14 -0500

    naming is key

--- Lab_2_Resistors_and_Diodes/data_and_code/experiment_one.m
+++ Lab_2_Resistors_and_Diodes/data_and_code/experiment_one.m
@@ -6,7 +6,6 @@ figure;
 semilogy( Vout_Exp1_1,Iin_Exp1_1,'*b','MarkerSize',2) %current-voltage
 hold on;
 semilogy(Vin_Exp1_2,Iout_Exp1_2, '*r','MarkerSize',2) %voltage-current
-hold on;
 % Instead of using the curve fitting toolbox, use polyfit to get values.
 fit = polyfit(Vin_Exp1_2, log(Iout_Exp1_2), 1);
 ut = abs(1/fit(1));
@@ -14,9 +13,9 @@ is = abs(exp(fit(2)));
 
 
 CV_theo = is .* exp(Vin_Exp1_2./ut);
-semilogy(Vout_Exp1_2,CV_theo,'-g','MarkerSize',5)
+semilogy(Vin_Exp1_2,CV_theo,'-k','MarkerSize',5)
 hold off;
-title( "TITLE");
+title( "Current-Voltage Characteristic");
 ylabel("Current (Amps)");
 xlabel("Voltage (Volts)");
 
@@ -25,4 +24,14 @@ legend('boxoff')
 legend('Location','northwest')
 
 
-hold off
+hold off;
+%% 
+clear
+
+load('collected_data.mat');
+
+incremental_resistance = diff(Vout_Exp1_1)./diff(Iin_Exp1_1);
+
+hold on;
+% Theoretical plot.
+loglog(Iin_Exp1_1(1,end-1), incremental_resistance);
