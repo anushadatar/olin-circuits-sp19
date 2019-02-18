a175c6424fe2f9b6909c7f8997819f7f8ecdc83c
Anusha Datar <anushadatar@gmail.com>
2019-02-18 13:18:40 -0500

    I just don't evenknow

--- Lab_2_Resistors_and_Diodes/data_and_code/experiment_two.m
+++ Lab_2_Resistors_and_Diodes/data_and_code/experiment_two.m
@@ -5,25 +5,25 @@ R100 = 100;
 R1K = 1000;
 R10K = 10000;
 p100 = polyfit(Vin_Exp2_100, log(Iout_Exp2_100), 1);
-Ut100 = real(1/p100(1));
-Is100 = real(exp(p100(2)));
+Ut100 = abs(1/p100(1));
+Is100 = abs(exp(p100(2)));
 
 p1K = polyfit(Vin_Exp2_1K, log(Iout_Exp2_1K), 1);
-Ut1K = 1/p1K(1);
-Is1K = exp(p1K(2));
+Ut1K = abs(1/p1K(1));
+Is1K = abs(esp(p1K(2)));
 
 p10K = polyfit(Vin_Exp2_100, log(Iout_Exp2_100), 1);
-Ut10K = 1/p10K(1);
-Is10K = exp(p10K(2));
+Ut10K = abs(1/p10K(1));
+Is10K = abs(exp(p10K(2)));
 
 Ion100 = Ut100/100
-Ion1K = Ut1K/1000;
-Ion10K = Ut10K/10000;
+Ion1K = abs(Ut1K/1000);
+Ion10K = abs(exp(Ut10K/10000));
 
-Von100 = Ut100 * log(Ion100/Is100);
+Von100 = abs(Ut100 * log(Ion100/Is100));
 VonIndex100 = 41;
-Von1K = Ut1K * log(Ion1K/Is1K);
-Von10K = Ut10K * log(Ion10K/Is10K);
+Von1K = abs(Ut1K * log(Ion1K/Is1K));
+Von10K = abs(Ut10K * log(Ion10K/Is10K));
 %% 
 % First plot : Voltage - Voltage
 % Experimental 
@@ -79,20 +79,28 @@ hold off;
 
 load('collected_data.mat');
 
+
 plot(Vin_Exp2_100, Iout_Exp2_100, '*r','MarkerSize',2);
 hold on;
 % Theoretical will be some piecewise function.
 % This is complicated becuase our Von is experimental as well.
 
 % TODO Theoretical fits for voltage-voltage plot.
-
-
+TheoreticalIout100 = zeros(numel(Vin_Exp2_100));
+for elem = 1:numel(Vin_Exp2_100)
+    if (Vin_Exp2_100(elem) < Von100) 
+        TheoreticalIout100(elem) = Is100 .* (Vin_Exp2_100(elem)/Ut100);
+    else 
+        TheoreticalIout100(elem) = (Vin_Exp2_100(elem) - Von100)/R100;
+    end
+end
+plot(Vin_Exp2_100, TheoreticalIout100);
 
 title( "Diode-Connected Transistor Current-Voltage Reponse - 100 Ohm Resistor");
 ylabel("Current Out (Amps)");
 xlabel("Voltage In (Volts)");
 
-legend('Experimental Current Response.');
+legend('Experimental Current Response', 'Theoretical Current Response');
 legend('boxoff')
 legend('Location','southeast')
 
@@ -108,6 +116,15 @@ hold on;
 % Theoretical will be some piecewise function.
 
 % TODO Theoretical fits for voltage-voltage plot.
+TheoreticalIout1K = zeros(numel(Vin_Exp2_1K));
+for elem = 1:numel(Vin_Exp2_1K)
+    if (Vin_Exp2_1K(elem) < Von1K) 
+        TheoreticalIout1K(elem) = Is1K .* (Vin_Exp2_1K(elem)/Ut1K);
+    else 
+        TheoreticalIout1K(elem) = (Vin_Exp2_1K(elem) - Von1K)/R1K;
+    end
+end
+plot(Vin_Exp2_1K, TheoreticalIout1K);
 
 
 
@@ -130,7 +147,15 @@ plot(Vin_Exp2_10K, Iout_Exp2_10K, '*r','MarkerSize',2);
 hold on;
 % Theoretical will be some piecewise function.
 
-% TODO Theoretical fits for voltage-voltage plot.
+TheoreticalIout10K = zeros(numel(Vin_Exp2_10K));
+for elem = 1:numel(Vin_Exp2_10K)
+    if (Vin_Exp2_10K(elem) < Von10K) 
+        TheoreticalIout1K(elem) = Is10K .* (Vin_Exp2_10K(elem)/Ut10K);
+    else 
+        TheoreticalIout10K(elem) = (Vin_Exp2_10K(elem) - Von10K)/R10K;
+    end
+end
+plot(Vin_Exp2_10K, TheoreticalIout10K);
 
 
 title( "Diode-Connected Transistor Current-Voltage Reponse - 10K Ohm Resistor");
@@ -168,7 +193,7 @@ title( "Voltage On as a Function of Resistance");
 ylabel("Voltage On (Volts)");
 xlabel("Resistance (Ohms)");
 
-legend('Experimental Voltage On', 'Theoreitcal Voltage On');
+legend('Experimental Voltage On', 'Theoretical Voltage On');
 legend('boxoff')
 legend('Location','northeast')
 
