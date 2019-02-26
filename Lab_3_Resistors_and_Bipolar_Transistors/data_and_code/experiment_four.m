4f040a7a25577b2fc761cbb30c5daab618e39132
Anusha Datar <anushadatar@gmail.com>
2019-02-26 16:28:54 -0500

    added collected data

---
+++ Lab_3_Resistors_and_Bipolar_Transistors/data_and_code/experiment_four.m
@@ -0,0 +1,25 @@
+clear;
+load('collected_data.mat');
+% Voltage Transfer Characteristic.
+plot(Vin_Exp3, Vout_Exp3);
+hold on;
+plot(Vin_Exp3, Vin_Exp3);
+
+plot(Vin_Exp4_200, Vout_Exp4_200);
+%plot(Vin_Exp4, Vout_Exp4_);
+
+plot(Vin_Exp4_300, Vout_Exp4_300);
+%plot(Vin_Exp4, Vout_Exp4);
+
+plot(Vin_Exp4_400, Vout_Exp4_400);
+%plot(Vin_Exp4, Vout_Exp4);
+
+title( "Common Emitter-Amplifer Transfer Characteristics");
+ylabel("Voltage Out (Volts)");
+xlabel("Voltage In (Volts)");
+
+legend('Experimental Emitter-Follower', 'Theoretical Emitter-Follower');
+legend('boxoff')
+legend('Location','southeast')
+
+hold off;
