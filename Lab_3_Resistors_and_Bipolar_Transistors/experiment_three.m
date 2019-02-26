28f18377d2d37b06ed5c0927363e28ea25be354b
Anusha Datar <anushadatar@gmail.com>
2019-02-26 16:06:16 -0500

    experiment 3

--- Lab_3_Resistors_and_Bipolar_Transistors/experiment_three.m
+++ Lab_3_Resistors_and_Bipolar_Transistors/experiment_three.m
@@ -3,13 +3,13 @@ load('collected_data.mat');
 % Voltage Transfer Characteristic.
 plot(Vin_Exp3, Vout_Exp3);
 hold on;
-plot(Vin_Exp3, Vinsm_Exp3);
+plot(Vin_Exp3, Vin_Exp3);
 
 title( "Emitter-Follower Voltage Transfer Characteristic");
 ylabel("Voltage Out (Volts)");
 xlabel("Voltage In (Volts)");
 
-legend('Experimental Voltage Transfer');
+legend('Experimental Voltage Transfer', 'Theoretical Voltage Transfer');
 legend('boxoff')
 legend('Location','southeast')
 
