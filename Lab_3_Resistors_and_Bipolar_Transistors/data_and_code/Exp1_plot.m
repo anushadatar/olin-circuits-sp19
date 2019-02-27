832466cbbc5f89c84089022e53a2eac1f78d2736
amfry <afry@olin.edu>
2019-02-27 10:42:00 -0500

    updates to plot1

--- Lab_3_Resistors_and_Bipolar_Transistors/data_and_code/Exp1_plot.m
+++ Lab_3_Resistors_and_Bipolar_Transistors/data_and_code/Exp1_plot.m
@@ -9,12 +9,15 @@ Ic = (-1.*(Ie)) - Ib;
 %% plot 1
 figure('Name','Plot 1')
 
+%Experimental
 semilogy(Vin_Exp1,Iout_Base_Exp1,'*');
 hold on;
 semilogy(Vin_Exp1, Ic,'*');
+% Theoretical TODO
 
-hold off;
 
+
+%Labels
 title( "Voltage vs. Current ");
 ylabel("Current (Amps)");
 xlabel("Voltage (Volts)");
@@ -23,34 +26,38 @@ legend('Base Current', 'Collector Current')
 legend('boxoff')
 legend('Location','northwest')
 
+hold off;
 %% plot 2
 figure('Name','Plot 2')
 
 Beta_Exp1 = Ic ./ Ib;
 
+%Experimental
 semilogx(Ib, Beta_Exp1);
 
-hold off;
+%Theoretical ToDo
 
+%Labels
 title( "Beta ");
 xlabel("Current (Amps)");
 ylabel("Beta Ratio");
 
-% legend('Calculated Beta Ratio')
-% legend('boxoff')
-% legend('Location','northwest')
+legend('Calculated Beta Ratio')
+legend('boxoff')
+legend('Location','northwest')
 
+hold off;
 %% Plot 3 rb = Ut/Ib
 figure('Name','Plot 3')
 
-get_fit = polyfit(Vin_Exp1, log(Ib), 1);
+get_fit = polyfit(Vin_Exp1(41:91), log(Ib(45:95)), 1);
 Ut = abs(1/get_fit(1));
 rb = Ut./(Ib);
 
 loglog(rb,Iout_Base_Exp1,'*')
 
 
-title( "rB");
+title( "Incremental Base Resistance (rb)");
 xlabel("Incremental Base Resistance (Ohms");
 ylabel("Base Current (Amps)");
 
@@ -64,9 +71,9 @@ figure('Name','Plot 4')
 
 gm = Ic./Ut;
 
-loglog(gm,Ic)
+loglog(gm,Ic,'*')
 
-% legend('Incremental Transconductance Gain')
-% legend('Transconductance Gain (Mhos)')
-% legend('Collector Current (Amps)')
+title('Incremental Transconductance Gain (gm)')
+xlabel('Transconductance Gain (Mhos)')
+ylabel('Collector Current (Amps)')
 hold off;
