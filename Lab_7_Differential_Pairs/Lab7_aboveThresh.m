f9215783a735f93f18beada466eafc11957d91a0
amfry <afry@olin.edu>
2019-04-13 19:59:42 -0400

    updates from lab 7

--- Lab_7_Differential_Pairs/Lab7_aboveThresh.m
+++ Lab_7_Differential_Pairs/Lab7_aboveThresh.m
@@ -7,23 +7,26 @@ Vdm = Vdm_big_range;
 Vcs = V_node_above_3V;
 %% Current Plot
 Idiff = I1 - I2;
+Imore = I1 + I2;
 
 figure;
-plot(Vdm,I1,'bo')
+plot(Vdm,I1,'b.')
 hold on;
-plot(Vdm,I2,'ro')
+plot(Vdm,I2,'r.')
 hold on;
-plot(Vdm,Idiff,'go')
+plot(Vdm,Idiff,'g.')
+hold on
+plot(Vdm,Imore,'m.')
 
 
 %Legend
-legend('I1','I2','I1-I2');
+legend('I1','I2','I1-I2','I1+I2');
 legend('boxoff')
 legend('Location','southeast')
 
 %Labels
-title('Current-Voltage Characteristic Above Threshold');
-xlabel("Vdm (Volts)");
+title('Differentail Pair Currents, Above Threshold (V_{b} = 1.5)');
+xlabel("V_{dm} (Volts)");
 ylabel("Current (Amps_)");
 
 hold off;
@@ -34,5 +37,5 @@ plot(Vdm,Vcs,'bo')
 
 %Labels
 title('Common-Source Voltage vs. Differential-Mode Voltage');
-xlabel("Vdm (Volts)");
-ylabel("Vcs (Volts)");
+xlabel("V_{dm} (Volts)");
+ylabel("V_{cs} (Volts)");
